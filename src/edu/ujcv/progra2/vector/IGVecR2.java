d9d1f6f7e05e8a3801e6fd295b316d90d3d73f74
DavidEscobar21 <DavidEscobar21@users.noreply.github.com>
2019-07-05 18:52:36 -0600

    agregue los setters y getters, tambien puse la suma y resta en los vectores de R2

--- src/edu/ujcv/progra2/vector/IGVecR2.java
+++ src/edu/ujcv/progra2/vector/IGVecR2.java
@@ -1,34 +1,57 @@
 package edu.ujcv.progra2.vector;
 
 public class IGVecR2 {
-private double x;
-private double y;
+    private double y;
+    private double x;
+
+public IGVecR2(){
+    this.x = 0;
+    this.y = 0;
+}
+public IGVecR2(double x, double y){
+    this.x = x;
+    this.y = y;
+    }
+    public IGVecR2(IGVecR2 b){
+    this.x = b.x;
+    this.y = b.y;
+    }
+
     //privado los datos
     // un escalar que represente 'x' y 'y' los componentes del vector
 
 
     // publico los constructores por defecto, copia, y conveniencia. recuerden a bob
 
+//contencion
 
-    // publico accesoras y mutadoras
-
+    // publico accesoras y mutadoras (getters and setters)
+    //    controla el acceso a la variable
+    //    controla como se cambia la variable
     //publico interfase
 
     //this es el vector a
     // c.x = a.x + b.x
     // c.y = a.y + b.y
     public IGVecR2 suma(IGVecR2 b){
+        IGVecR2 a1 = new IGVecR2();
+        a1.x = x + b.x;
+        a1.y = y + b.y;
+        return a1;
         //TODO: implementar
-        return new IGVecR2();
     }
 
     public IGVecR2 resta(IGVecR2 b){
         //TODO: implementar
-        return new IGVecR2();
+        IGVecR2 a1 = new IGVecR2();
+        a1.x = x - (b.x);
+        a1.y = y - (b.y);
+        return a1;
     }
 
     //producto.
     public  IGVecR2 escalarPorVector(double alpha){
+
         //TODO: implementar
         return new IGVecR2();
     }
@@ -59,5 +82,20 @@ private double y;
         return Math.sqrt(productoPunto(this));
     }
 
+    public double getX() {
+        return x;
+    }
 
+    public void setX(double x) {
+        this.x = x;
+    }
+
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
